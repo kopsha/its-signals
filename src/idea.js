608fa433141b51b5a45f2f52165e2af4c12276d3
Florin Ciurcanu <florin.ciurcanu@gmail.com>
2024-10-03 14:03:00 +0300

    How about this?

--- src/idea.js
+++ src/idea.js
@@ -4,20 +4,41 @@ class Signal {
     }
 
     connect(receiver, slot) {
-        if (
-            typeof receiver[slot.name] === "function" &&
-            slot === receiver[slot.name]
-        ) {
-            this.slots.push(slot.bind(receiver))
+        if (typeof slot === "function") {
+            const alreadyConnected = this.slots.some(
+                (s) => s.slot === slot && s.receiver === receiver,
+            )
+            if (alreadyConnected) {
+                console.warn(
+                    `${slot.name} is already connected to this signal.`,
+                )
+            } else {
+                const boundSlot = slot.bind(receiver)
+                this.slots.push({ receiver, slot, boundSlot })
+            }
         } else {
             console.error(
-                `${slot.name} is not a valid function on the receiver ${receiver.constructor.name}.`,
+                `${slot.name} is not a valid function of ${receiver.constructor.name}.`,
             )
         }
     }
 
     emit(...args) {
-        this.slots.forEach((slot) => slot(...args))
+        this.slots.forEach(({ boundSlot }) => {
+            queueMicrotask(() => boundSlot(...args))
+        })
+    }
+
+    disconnect(receiver, slot) {
+        if (receiver && slot) {
+            this.slots = this.slots.filter(
+                (s) => s.receiver !== receiver || s.slot !== slot,
+            )
+        } else if (receiver) {
+            this.slots = this.slots.filter((s) => s.receiver !== receiver)
+        } else {
+            this.slots = []
+        }
     }
 }
 
@@ -28,16 +49,20 @@ class Button extends Signal {
         this.htmlElement.addEventListener("click", () => {
             const now = Date.now()
             this.emit("Hello from button", now)
+            console.log("suppose the emitter code is finished")
         })
     }
 }
 
 class App {
+    #counter = 0
+
     constructor(button) {
         button.connect(this, this.onButtonClicked)
     }
 
     onButtonClicked(...messages) {
-        console.log("Signal received:", messages)
+        this.#counter++
+        console.log("Signal received:", messages, this.#counter, "times.")
     }
 }
