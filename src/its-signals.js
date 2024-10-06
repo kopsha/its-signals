276cddc323eee05f2bb5a96a55ffd3e7db1aed5f
Florin Ciurcanu <kopsha@users.noreply.github.com>
2024-10-06 13:25:32 +0300

    Added proper tests and simplified usage. (#7)

--- src/its-signals.js
+++ src/its-signals.js
@@ -5,76 +5,43 @@ export default class Signal {
         console.assert(element instanceof EventTarget)
         let instance = new Signal()
         element.addEventListener(event, e => instance.emit(e.target.value))
+        // TODO: Add mechanics to remove this listener
         return instance
     }
 
-    connect(receiver, slot) {
-        if (!receiver || !slot) {
-            throw new Error("Expected both receiver and slot arguments.")
-        } else if (typeof slot !== "function") {
-            throw new Error(`${slot} is not a function of ${receiver.constructor.name}.`)
-        }
-
-        const alreadyConnected = this.#slots.some(
-            s => s.slot === slot && s.receiver === receiver
-        )
-        if (alreadyConnected) {
-            // NOTE: ignore multiple connections for now
-            console.warn(`${slot.name} is already connected to ${this.costructor.name} signal.`)
-        }
-        const boundSlot = slot.bind(receiver)
-        this.#slots.push({ receiver, slot, boundSlot })
+    isConnected() {
+        return Boolean(this.#slots.length)
     }
 
-    isConnected() {
-        return Boolean(this.#slots)
+    connect(slot) {
+        if (slot instanceof Signal) {
+            slot = slot.emit
+        } else if (slot instanceof Function) {
+            // TODO: check if bounding this is necessary here
+        } else {
+            throw new Error("Expected a slot method or a Signal instance.")
+        }
+
+        const alreadyConnected = this.#slots.some(si => si === slot)
+        if (alreadyConnected) {
+            // NOTE: ignore multiple connections for now
+            console.warn(`${slot.name} is already connected to signal.`)
+        } else {
+            this.#slots.push(slot)
+        }
     }
 
     emit(...args) {
-        this.#slots.forEach(({ boundSlot }) => {
-            queueMicrotask(() => boundSlot(...args))
-        })
+        for (const slot of this.#slots) {
+            queueMicrotask(() => slot(...args))
+        }
     }
 
-    disconnect(slot = null, receiver = null) {
-        if (receiver && slot) {
-            this.#slots = this.#slots.filter(s => s.receiver !== receiver || s.slot !== slot)
-        } else if (receiver) {
-            this.#slots = this.#slots.filter(s => receiver !== s.receiver)
+    disconnect(slot = null) {
+        if (slot) {
+            this.#slots = this.#slots.filter(si => si !== slot)
         } else {
             this.#slots = []
         }
-    }
-}
-
-class InterestedHandler {
-    finished = new Signal()
-    #left = 0
-    #right = 0
-
-    constructor() {
-        this.finished.connect(this, this.onFinished)
-    }
-
-    onAgeChanged(...messages) {
-        this.#left++
-        console.log("age changed:", messages, this.#left, "times.")
-        if (this.#left && this.#right) {
-            this.finished.emit()
-            this.finished.disconnect()
-        }
-    }
-
-    onReaction(...messages) {
-        this.#right++
-        console.log("new action:", messages, this.#right, "times.")
-        if (this.#left && this.#right) {
-            this.finished.emit()
-            this.finished.disconnect()
-        }
-    }
-
-    onFinished(...messages) {
-        console.log("finished...")
     }
 }
